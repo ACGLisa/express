cafe33bc29faaca24a4a04955e8193fefd563599
ACGLisa <49675556+ACGLisa@users.noreply.github.com>
2019-05-21 18:04:25 -0700

    Lab 11

--- index.js
+++ index.js
@@ -12,4 +12,66 @@ app.get("/hi", (request, response) => {
 	response.send("Hello world!");
 });
 
+const CONNECTION_URL = "mongodb+srv://root:<password>@lab10-twodd.mongodb.net/test?retryWrites=true";
+const DATABASE_NAME = "Lab10";
+
+app.post("/notes", (request, response) => {
+	MongoClient.connect(CONNECTION_URL, {useNewUrlParser: true}, (error, client) => {
+		if(error) {
+			response.send(error);
+			throw error;
+		}
+		database = client.db(DATABASE_NAME);
+		collection = database.collection("Notes");
+
+		collection.insert(request.body, (error, result) => {
+			if(error) {
+				return response.status(500).send(error);
+			}
+			response.send(result.result);
+		});
+	});
+});
+
+app.get("/notes", (request, response) => {
+	MongoClient.connect(CONNECTION_URL, {useNewUrlParser: true}, (error, client) => {
+		if(error) {
+			response.send(error);
+			throw error;
+		}
+		database = client.db(DATABASE_NAME);
+		collection = database.collection("Notes");
+
+		collection.find({}).toArray((error, result) => {
+			if(error) {
+				return response.status(500).send(error);
+			}
+			response.send(result);
+		});
+	});
+});
+
+app.get("/notes/:id", (request, response) => {
+	MongoClient.connect(CONNECTION_URL, {useNewUrlParser: true}, (error, client) => {
+		if(error) {
+			response.send(error);
+			throw error;
+		}
+		database = client.db(DATABASE_NAME);
+		collection = database.collection("Notes");
+
+		collection.find({}).toArray((error, result) => {
+			if(error) {
+				return response.status(500).send(error);
+			}
+
+			var numberID = parseInt(request.params.id);
+			if(numberID >= result.length)
+			   response.send("Not enongh elements in database");
+			else
+				response.send(result[numberID]);
+		});
+	});
+});
+.
 module.exports = app;
